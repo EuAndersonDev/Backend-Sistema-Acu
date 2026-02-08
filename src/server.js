6512143e20fde5588be456d7592854b2617b3966
EuAndersonDev <andersonaugustorei1227@gmail.com>
2026-02-07 22:44:07 -0300

    feat, config cors for pc client

--- src/server.js
+++ src/server.js
@@ -5,9 +5,25 @@ const routes = require('./routes');
 const db = require('./models');
 
 const app = express();
-
 // Middlewares
-app.use(cors());
+const whitelist = [
+  'http://localhost:3333',
+  'http://localhost:5173',
+  'http://192.168.18.156:5173',
+  'http://192.168.18.156',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
